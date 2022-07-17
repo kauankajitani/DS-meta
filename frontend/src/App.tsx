2c1940a40ff9386d841a578ab37580b922491c11
Kauan Kajitani Silva <kauankajitani02@gmail.com>
2022-07-17 16:47:00 -0300

    setMinDate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,9 @@
-import NotificationButton from "./components/NotificationButton"
-import Header from "./components/Header"
-import SalesCard from "./components/SalesCard"
+import NotificationButton from "./components/NotificationButton";
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+
+
+
 function App() {
     return (
         <>
@@ -16,4 +19,4 @@ function App() {
     )
 }
 
-export default App
+export default App;
